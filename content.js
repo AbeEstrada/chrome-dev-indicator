2f4b2504e99b12b4bcc30f3249dfd22f610a929f
Abraham Estrada <abraham.estrada@gmail.com>
2017-12-07 14:45:36 -0700

    Experimental Firefox support

--- content.js
+++ content.js
@@ -1,7 +1,24 @@
-const defaultIndicator = '☕️'
+var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+const defaultIndicator = "☕️";
 
-chrome.storage.sync.get({
-  indicator: defaultIndicator
-}, (items) => {
-  document.title = `${items.indicator} ${document.title}`
-})
+if (isChrome) {
+  chrome.storage.sync.get(
+    {
+      indicator: defaultIndicator
+    },
+    items => {
+      setTitle(items.indicator);
+    }
+  );
+} else {
+  // Firefox?
+  const gettingIndicator = browser.storage.sync.get("indicator");
+  gettingIndicator.then(res => {
+    const indicator = res.indicator == undefined ? defaultIndicator : res.indicator;
+    setTitle(indicator);
+  });
+}
+
+function setTitle(indicator) {
+  document.title = `${indicator} ${document.title}`;
+}
--- options.js
+++ options.js
@@ -1,25 +1,31 @@
-const defaultIndicator = '☕️'
+const defaultIndicator = "☕️";
 
-const saveOptions = (e) => {
-  const indicator = document.getElementById('indicator').value
-  chrome.storage.sync.set({
-    indicator: indicator || defaultIndicator
-  }, () => {
-    const status = document.getElementById('status')
-    status.style.opacity = 1
-    setTimeout(() => {
-      status.style.opacity = 0
-    }, 1000)
-  })
-}
+const saveOptions = e => {
+  const indicator = document.getElementById("indicator").value;
+  browser.storage.sync.set(
+    {
+      indicator: indicator || defaultIndicator
+    },
+    () => {
+      const status = document.getElementById("status");
+      status.style.opacity = 1;
+      setTimeout(() => {
+        status.style.opacity = 0;
+      }, 1000);
+    }
+  );
+};
 
 const restoreOptions = () => {
-  chrome.storage.sync.get({
-    indicator: defaultIndicator
-  }, (items) => {
-    document.getElementById('indicator').value = items.indicator
-  })
-}
+  browser.storage.sync.get(
+    {
+      indicator: defaultIndicator
+    },
+    items => {
+      document.getElementById("indicator").value = items.indicator;
+    }
+  );
+};
 
-document.addEventListener('DOMContentLoaded', restoreOptions)
-document.getElementById('save').addEventListener('click', saveOptions)
+document.addEventListener("DOMContentLoaded", restoreOptions);
+document.getElementById("save").addEventListener("click", saveOptions);
