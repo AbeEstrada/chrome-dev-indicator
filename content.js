daa20aace30b2f64c2f402424c14e5a90d97603a
Abraham Estrada <abraham.estrada@gmail.com>
2018-05-17 20:05:33 -0600

    v1.2
    - This version removes `*.dev` and `*.local` and use recommended `*.test` TLD by RFC2606

--- content.js
+++ content.js
@@ -1,23 +1,13 @@
-var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
 const defaultIndicator = "☕️";
 
-if (isChrome) {
-  chrome.storage.sync.get(
-    {
-      indicator: defaultIndicator
-    },
-    items => {
-      setTitle(items.indicator);
-    }
-  );
-} else {
-  // Firefox?
-  const gettingIndicator = browser.storage.sync.get("indicator");
-  gettingIndicator.then(res => {
-    const indicator = res.indicator == undefined ? defaultIndicator : res.indicator;
-    setTitle(indicator);
-  });
-}
+chrome.storage.sync.get(
+  {
+    indicator: defaultIndicator
+  },
+  items => {
+    setTitle(items.indicator);
+  }
+);
 
 function setTitle(indicator) {
   document.title = `${indicator} ${document.title}`;
